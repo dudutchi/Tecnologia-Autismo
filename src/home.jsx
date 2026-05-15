e0ceaf56ccebed2c20b49f8b224ddfe38e974e65
Jean Souza <jean.souza.maccao@gmail.com>
2026-05-14 22:54:36 -0300

    ajustei o bug da página principal

--- src/home.jsx
+++ src/home.jsx
@@ -2,7 +2,7 @@ import ModoLivre from "./ModoLivre";
 
 export default function Home() {
   return (
-      <main className="w-full lg:ml-72 bg-gray-50 min-h-screen">
+      <main className="w-full bg-gray-50 min-h-screen">
         <ModoLivre />
       </main>
   );
