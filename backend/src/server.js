7304032fcd9258869eafafc37e257a60aff02000
Eduardo Furtado <furtadoe834@gmail.com>
2026-05-15 16:46:39 -0300

    alterei o dns do node

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,9 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import reminderRoutes from "./routes/reminderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 dotenv.config();
 
